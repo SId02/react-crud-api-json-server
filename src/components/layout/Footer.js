13bcd19413583d73e44cbc531c672345a7e1489d
SId02 <31335947+SId02@users.noreply.github.com>
2022-04-09 15:10:22 +0530

    update

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-5 bg-dark">
+    <div className="container">
+      <div className="pt-2 text-center  text-white">
+        <p>All rights reserved  2021</p>
+      </div>
+    </div>
+  </footer>
+  );
+};
+
+export default Footer;
+
+
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        <Link className="navbar-brand" href="/">
+          Recat User
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <Link className="btn btn-outline-light" to="/users/add">
+       
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg>
+          Add User</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+
